c5b3de8952d33e70a1f69c0c7a4d4e595ed1d5e9
functorial <alexpokorny92@gmail.com>
2021-06-27 17:15:10 -0700

    Add ARMA model

--- TimeSeriesHelpers.py
+++ TimeSeriesHelpers.py
@@ -1,5 +1,6 @@
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.ar_model import AutoReg
+from statsmodels.tsa.arima.model import ARIMA
 from math import sqrt
 from sklearn.metrics import mean_squared_error
 
@@ -18,7 +19,79 @@ def adf_summary(X):
     else:
         print("Fail to reject null: Time series may not be stationary.")
 
-def eval_AR(lags, train, val):
+
+def eval_ARMA(AR_lags:int, MA_lags:int, train, val):
+    '''Creates and trains an ARMA model with the 
+    supplied significant lags. Returns the root mean 
+    squared error of the model predictions against the 
+    supplied validation set.''' 
+
+    model = ARIMA(train, order=(AR_lags, 0, MA_lags), enforce_stationarity=False, enforce_invertibility=False)
+    model_fit = model.fit()
+    val_hat = model_fit.forecast(len(val))
+    rmse = sqrt(mean_squared_error(val, val_hat))
+    return rmse 
+
+def rolling_eval_ARMA(AR_lags:int, MA_lags:int, X, h:int, window_h_ratio:int=5):
+    '''Performs a rolling window analysis on an 
+    ARMA model with the suppllied significant lags. 
+    X is the full time series, h is the size of the forecast horizon. 
+
+    Returns the pre-live fitted model, the RMSE mean, and the
+    RMSE variance over the windows.
+
+    Note: If len(X) is not divisible by h, then the head of X
+    is sliced off in the smallest way possible so that the
+    result is divisible by h.'''
+
+    m = h*window_h_ratio        # The size of the rolling window.
+    idx = (len(X) - m) % h      # The index used to slice X.        
+    T = len(X) - idx            # Sample size after slice.
+    N = ((T-m)//h)+1            # Number of windows.
+    assert m <= len(X), 'Window size must be smaller than time series.'
+
+    # Walk forward validation
+    rmse_vals = []
+    for i in range(N):
+        a = h*i     # Start index for the window
+        train = X[idx+a:idx+a+m-h]
+        val = X[idx+a+m-h:idx+a+m]
+        rmse = eval_ARMA(AR_lags, MA_lags, train, val)
+        rmse_vals.append(rmse)
+        rmse_avg = sum(rmse_vals) / N
+        rmse_var = sum([(e - rmse_avg)**2 for e in rmse_vals]) / (N-1)
+
+    # Pre-Live Optimization
+    train = X[-m:]
+    model = ARIMA(train, order=(AR_lags, 0, MA_lags), enforce_stationarity=False, enforce_invertibility=False)
+    model_fit = model.fit()
+
+    return model_fit, rmse_avg, rmse_var
+
+def compare_ARMA_models(AR_lag_sets:list, MA_lag_sets:list, X, h:int, window_h_ratio:int=5):
+    '''Performs a Walk Forward Analysis for predictive performance
+    for each set of significant lags given. 
+    `AR_lag_sets` is a list of ints of significant lags for the AR part.
+    `MA_lag_sets` is a list of ints of significant lags for the MA part.
+    `X` is the time series.
+    `h` is the forecast horizon size.'''
+    m = h*window_h_ratio        # The size of the rolling window.
+    idx = (len(X) - m) % h      # The index used to slice X.        
+    T = len(X) - idx            # Sample size after slice.
+    N = ((T-m)//h)+1            # Number of windows.
+    assert m <= len(X), 'Window size must be smaller than time series.'
+    print(f"Window Size: {m}\tNum Windows: {N}\n")
+
+    i = 0
+    for p in AR_lag_sets:
+        for q in MA_lag_sets:
+            i += 1
+            print(f"Test {i}: ARMA(p={p}, q={q}))\n========================================================")
+            _, rmse_avg, rmse_var = rolling_eval_ARMA(AR_lags=p, MA_lags=q, X=X, h=h, window_h_ratio=window_h_ratio)
+            print(f"RMSE Mean: {rmse_avg:.2f}\tRMSE Variance: {rmse_var:.2f}\n")
+
+
+def eval_AR(lags:list, train, val):
     '''Creates and trains an AutoRegressive model with the 
     supplied significant lags. Returns the root mean 
     squared error of the model predictions against the 
@@ -30,7 +103,7 @@ def eval_AR(lags, train, val):
     rmse = sqrt(mean_squared_error(val, val_hat))
     return rmse 
 
-def rolling_eval_AR(lags, X, h, window_h_ratio=5):
+def rolling_eval_AR(lags:list, X, h:int, window_h_ratio:int=5):
     '''Performs a rolling window analysis on an 
     AutoRegressive model with the suppllied significant lags. 
     X is the full time series, h is the size of the forecast horizon. 
@@ -66,7 +139,7 @@ def rolling_eval_AR(lags, X, h, window_h_ratio=5):
 
     return model_fit, rmse_avg, rmse_var
 
-def compare_AR_models(lag_sets, X, h, window_h_ratio=5):
+def compare_AR_models(lag_sets:list, X, h:int, window_h_ratio:int=5):
     '''Performs a Walk Forward Analysis for predictive performance
     for each set of significant lags given. 
     `lag_sets` is a list of lists of significant lags.
